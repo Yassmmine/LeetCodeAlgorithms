28efce57ca0ba4e34149c4c448a06bc1d41dfd28
Yassmmine <110379746+Yassmmine@users.noreply.github.com>
2023-02-18 23:21:18 +0200

    Time: 84 ms (64.97%), Space: 39.6 MB (22.29%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -1,7 +1,23 @@
 public class Solution {
     public string ReverseWords(string s) {
-           var words = s.Split(' ');
-            var wordsList=words.Where(a=>!string.IsNullOrEmpty(a)).Reverse().ToList();
-            return String.Join(" ", wordsList);
+         Stack<string> reverse = new Stack<string>(); //Space: O(n)
+            StringBuilder sb = new StringBuilder(s.Trim()); //Space: O(n), Trim input string as we need ignore spaces at the start and end.
+
+            for (int i = 0; i < sb.Length; i++) //Time: O(n)
+            {
+                StringBuilder word = new StringBuilder(); //always use StringBuilder if using string inside a loop
+                while (i < sb.Length && sb[i] != ' ') //travel till you find next space, this will crop each word for you
+                {
+                    word.Append(sb[i]);
+                    i++;
+                }
+                reverse.Push(word.ToString()); //add cropped word to stack
+
+                while (i < sb.Length && sb[i + 1] == ' ') i++; //ignore extra spaces, we only care about 1 space, if there are 4 or 10000 spaces we are going to ignore all of them
+
+                if (i < sb.Length) reverse.Push(" "); //add one space after each word          
+            }
+            return String.Join("", reverse); //Time: O(n):  just join all the string from stack and return result
+
     }
 }
